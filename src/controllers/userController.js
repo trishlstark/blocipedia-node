b609d99479353a2796442b6c490d9e3075da73d4
Trisha Stark <trishlstark@gmail.com>
2018-11-28 15:58:28 -0700

    Sign in and sign up complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,5 +28,38 @@ module.exports = {
         });
     },
 
+    signInForm(req, res, next){
+        res.render("users/sign_in");
+    },
+
+    signIn(req,res,next){
+        passport.authenticate("local")(req,res,function () {
+            if(!req.user){
+                req.flash("notice", "Sign in failed. Please try again");
+                res.redirect("/users/sign_in");
+            }else{
+                req.flash("notice", "You've successfully signed in!");
+                res.redirect("/");
+            }
+        })
+    },
+
+    signOut(req,res,next){
+        req.logout();
+        req.flash("notice", "You've successfully signed out!");
+        res.redirect("/");
+    },
+    show(req, res, next){
+         userQueries.getUser(req.params.id, (err, result) => {
+     
+           if(err || result.user === undefined){
+             req.flash("notice", "No user found with that ID.");
+             res.redirect("/");
+           } else {
+             res.render("users/show", {...result});
+           }
+         });
+       }
+
     
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,5 +7,10 @@ const userController = require("../controllers/UserController");
 
 router.get("/users/signup", userController.signUp);
 router.post("/users/signup", validation.validateUsers, userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
+router.get("/users/:id", userController.show);
+
 
 module.exports = router;
